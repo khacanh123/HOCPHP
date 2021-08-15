ea5f52fe0c4635c0b00ebf3449be2d97c5a17a54
khacanh123 <74588295+khacanh123@users.noreply.github.com>
2021-08-15 22:08:45 +0700

    Create BTap.php

---
+++ BTap.php
@@ -0,0 +1,10 @@
+ // Bài mot: dùng lệnh switch ... case để chuyển các sô tròn chục nhỏ hơn 80 --> chuoi
+    // bài 2: Cho mảng sau: ['abc', 4, 'bcd', 6, 'abd', 5];
+    // + tìm các phần tử là số trong mảng
+    // + trả về chuỗi khi ghép các phần tử không phải là số trong mảng
+    // + Tìm vị trí của chuỗi cuối cùng trong mảng
+    // Bài 3: Nhập vào một ngày có định dạng dd/mm/YY. Ngày nhập vào phải nhỏ hơn 25/08/2020(Nếu sai thì báo lỗi)
+    // + kiểm tra xem ngày đó vào mùa nào trong năm
+    // + kiểm tra xem ngày đó có bằng 25/08/2020 hay không, nếu không thì tính số ngày để đến ngày đó 
+    // (mot tháng = 30).
+    // Deadline: 22/08
